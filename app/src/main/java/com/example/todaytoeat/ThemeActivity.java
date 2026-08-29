bb3abea32c17784fb801ac23e9d734454986e568
chenbrush <2539228657@qq.com>
2026-08-29 21:54:02 +0800

    添加了 ThemeActivity ，目前只对activity的设计做了初步规划

---
+++ app/src/main/java/com/example/todaytoeat/ThemeActivity.java
@@ -0,0 +1,24 @@
+package com.example.todaytoeat;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ThemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_theme);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.theme_activity), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
